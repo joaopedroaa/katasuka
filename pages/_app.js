d4fc750b1704a680f2e49e2509da4cce8e3de470
João Pedro Alvarenga <joaopedroaats@pm.me>
2022-05-22 01:59:04 -0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
+import * as gtag from '../lib/gtag'
+import Analytics from '../components/Analytics'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Analytics />
+    </>
   )
 }
 
